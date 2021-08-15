36faca8458587e1b13c3e7cf6469afb2dfa85bca
Joe Harrison <josephharrison1@me.com>
2021-08-15 20:19:22 +0100

    changed rainbow old one was broke

--- src/colour.rs
+++ src/colour.rs
@@ -1,7 +1,18 @@
+#![allow(dead_code)]
 use crate::lightstrip::Pixel;
 
 pub const RED: Pixel = (255, 0, 0);
+pub const ORANGE: Pixel = (255, 127, 0);
+pub const YELLOW: Pixel = (255, 255, 0);
+pub const LIME: Pixel = (127, 255, 0);
 pub const GREEN: Pixel = (0, 255, 0);
+pub const AQUA: Pixel = (0, 255, 127);
+pub const CYAN: Pixel = (0, 255, 255);
+pub const LIGHTBLUE: Pixel = (0, 127, 255);
 pub const BLUE: Pixel = (0, 0, 255);
+pub const PURPLE: Pixel = (127, 0, 255);
+pub const MAGENTA: Pixel = (255, 0, 255);
+pub const PINK: Pixel = (255, 0, 127);
+
 pub const WHITE: Pixel = (255, 255, 255);
 pub const OFF: Pixel = (0, 0, 0);
--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,14 @@
 mod colour;
 mod lightstrip;
 use rppal::spi::{Bus, Mode, SlaveSelect, Spi};
+use std::{thread, time};
 
 fn main() {
     let mut spi = Spi::new(Bus::Spi0, SlaveSelect::Ss0, 6_400_000, Mode::Mode0).unwrap();
     let mut lights = lightstrip::Strip::new(30, &mut spi);
-    let mut c: (u8, u8, u8) = (255, 0, 0);
     loop {
-        // red -> yellow
-        for _ in 0..256 {
-            c.1 += 1;
-            lights.update();
-        }
-        //(255,255,0)
-
-        // yellow -> green
-        for _ in 0..256 {
-            c.0 -= 1;
-            lights.update();
-        }
-        //(0,255,0)
-
-        // green -> cyan
-        for _ in 0..256 {
-            c.2 += 1;
-            lights.update();
-        }
-        //(0,255,255)
-
-        //cyan->blue
-        for _ in 0..256 {
-            c.1 -= 1;
-            lights.update();
-        }
-        //(0,0,255)
-
-        //blue -> magenta
-        for _ in 0..256 {
-            c.0 += 1;
-            lights.update();
-        }
-        //(255,0,0)
-
-        //magenta -> red
-        for _ in 0..256 {
-            c.2 -= 1;
-            lights.update();
-        }
+        lights.push(colour::RED);
+        lights.update();
+        thread::sleep(time::Duration::from_millis(250));
     }
 }
