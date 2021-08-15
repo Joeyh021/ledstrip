e508ef24efbe9ff7235da30acae1394d67b15a93
Joe Harrison <josephharrison1@me.com>
2021-08-15 18:47:56 +0100

    added some colour constants

---
+++ src/colour.rs
@@ -0,0 +1,7 @@
+use crate::lightstrip::Pixel;
+
+pub const RED: Pixel = (255, 0, 0);
+pub const GREEN: Pixel = (0, 255, 0);
+pub const BLUE: Pixel = (0, 0, 255);
+pub const WHITE: Pixel = (255, 255, 255);
+pub const OFF: Pixel = (0, 0, 0);
