3f046d11a2d4a229509bd7c11eeb36487daaef0f
Joe Harrison <josephharrison1@me.com>
2021-08-15 20:21:42 +0100

    further rainbow updates

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,39 @@ fn main() {
     loop {
         lights.push(colour::RED);
         lights.update();
-        thread::sleep(time::Duration::from_millis(250));
+        thread::sleep(time::Duration::from_millis(100));
+        lights.push(colour::ORANGE);
+        lights.update();
+        thread::sleep(time::Duration::from_millis(100));
+        lights.push(colour::YELLOW);
+        lights.update();
+        thread::sleep(time::Duration::from_millis(100));
+        lights.push(colour::LIME);
+        lights.update();
+        thread::sleep(time::Duration::from_millis(100));
+        lights.push(colour::GREEN);
+        lights.update();
+        thread::sleep(time::Duration::from_millis(100));
+        lights.push(colour::AQUA);
+        lights.update();
+        thread::sleep(time::Duration::from_millis(100));
+        lights.push(colour::CYAN);
+        lights.update();
+        thread::sleep(time::Duration::from_millis(100));
+        lights.push(colour::LIGHTBLUE);
+        lights.update();
+        thread::sleep(time::Duration::from_millis(100));
+        lights.push(colour::BLUE);
+        lights.update();
+        thread::sleep(time::Duration::from_millis(100));
+        lights.push(colour::PURPLE);
+        lights.update();
+        thread::sleep(time::Duration::from_millis(100));
+        lights.push(colour::MAGENTA);
+        lights.update();
+        thread::sleep(time::Duration::from_millis(100));
+        lights.push(colour::PINK);
+        lights.update();
+        thread::sleep(time::Duration::from_millis(100));
     }
 }
