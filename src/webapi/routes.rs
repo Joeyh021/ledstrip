2f3fb6cd30a5d83bcb99ea8f6a93bd800057f442
Joeyh021 <joeyh021@icloud.com>
2021-08-30 21:09:48 +0100

    fixed hex parser

--- src/webapi/routes.rs
+++ src/webapi/routes.rs
@@ -44,9 +44,9 @@ fn parse_hex_code(hex: &str) -> Option<Pixel> {
         return None;
     };
     let num = u32::from_str_radix(hex, 16).ok()?;
-    let r = (num & 0xff).try_into().ok()?;
+    let b = (num & 0xff).try_into().ok()?;
     let g = ((num & 0xff00) >> 8).try_into().ok()?;
-    let b = ((num & 0xff0000) >> 16).try_into().ok()?;
+    let r = ((num & 0xff0000) >> 16).try_into().ok()?;
     Some((r, g, b))
 }
 
@@ -60,6 +60,6 @@ mod test {
         assert_eq!(RED, parse_hex_code("ff0000").unwrap());
         assert_eq!(GREEN, parse_hex_code("00ff00").unwrap());
         assert_eq!(BLUE, parse_hex_code("0000ff").unwrap());
-        assert_eq!((12, 34, 56), parse_hex_code("123456").unwrap());
+        assert_eq!((0x12, 0x34, 0x56), parse_hex_code("123456").unwrap());
     }
 }
