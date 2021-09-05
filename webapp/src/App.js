e02a5dbb2666cfcb2a8be975c63d42ff2b0e1406
Joe Harrison <josephharrison1@me.com>
2021-09-05 19:07:12 +0100

    simple colour picker added

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -5,15 +5,25 @@ import { HexColorPicker } from "react-colorful";
 
 const ColourPicker = () => {
   const [color, setColor] = useState("#aabbcc");
-  return <HexColorPicker color={color} onChange={setColor} />;
+  return (
+    <div>
+      <HexColorPicker color={color} onChange={setColor} />{" "}
+      <button onClick={() => send_colour(color)}> Update</button>
+    </div>
+  );
 };
 
 function App() {
   return (
-    <div className="App" class="center">
-      <ColourPicker colour="ffffff" />
+    <div className="App">
+      <ColourPicker />
     </div>
   );
 }
 
+function send_colour(colour) {
+  fetch("/on")
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+}
 export default App;
